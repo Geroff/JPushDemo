0d8f029fc6d51020203bd6a6b9acdcd9771f357e
lgf <910683417@qq.com>
2018-07-19 14:07:40 +0800

    添加设置通知栏图标

--- app/src/main/java/com/juanvision/jpush/demo/MyApplication.java
+++ app/src/main/java/com/juanvision/jpush/demo/MyApplication.java
@@ -1,7 +1,9 @@
 package com.juanvision.jpush.demo;
 
 import android.app.Application;
+import android.app.Notification;
 
+import cn.jpush.android.api.BasicPushNotificationBuilder;
 import cn.jpush.android.api.JPushInterface;
 
 public class MyApplication extends Application {
@@ -10,5 +12,11 @@ public class MyApplication extends Application {
         super.onCreate();
         JPushInterface.setDebugMode(true);
         JPushInterface.init(getApplicationContext());
+        // 设置通知栏样式，处理在google手机上通知栏显示为黑色图标的问题
+        BasicPushNotificationBuilder builder = new BasicPushNotificationBuilder(getApplicationContext());
+        builder.statusBarDrawable = R.mipmap.ic_launcher;
+        builder.notificationFlags = Notification.FLAG_AUTO_CANCEL;  //可选，设置为可点击消失
+        builder.notificationDefaults = Notification.DEFAULT_SOUND; //可选，设置声音提示
+        JPushInterface.setDefaultPushNotificationBuilder(builder);
     }
 }
